0882feea6fa1bf0976e42375996f71794650884c
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-07-04 22:37:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//Create a function that takes an array containing only numbers and return the first element.
+
+
+function returnFirstElement(arr) {
+    return arr[0];
+}
+
+console.log(returnFirstElement([1, 2, 3, 4]));
